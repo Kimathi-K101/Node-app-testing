3acf39d4da93f39a7eb864be83b16b3b49a59710
Kimathi <kiprotich.kaai@gmail.com>
2018-09-05 01:43:57 -0400

    Using the expect assertion library to make automated testing easier

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,15 @@
 module.exports.add = (a,b) => a + b;
+
+module.exports.asyncAdd = (a,b, callback) => {
+  setTimeout(() => {
+    callback(a+b)
+  }, 1000);
+};
+
 module.exports.square = (a) => a*a;
+module.exports.setName = (user, fullName) => {
+  var names = fullName.split(' '); //Split on the space into an array
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,37 @@
+const expect = require('expect');
 const utils = require('./utils.js');
 
-//A function provided by mocha. lets us define a test case with two arguements
-//The keyword 'it' plays into the description. This is called Behaviour Driven Development (BDD)
 it('should add two numbers', () => {
-  var results = utils.add(33,11);
-  if(results !== 44){
-    throw new Error(`Expected 44 but got ${results}`);
-  }
+  var result = utils.add(33,11);
+  expect(result).toBe(44).toBeA('number');
+});
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4,3, (sum) => {
+    expect(sum).toBe(10).toBeA('number');
+    done();
+  });
 });
 
 it('should square a number', () => {
   var result = utils.square(2);
-  if(result !== 4){
-    throw new Error(`Expected 4 but got ${results}`);
-  }
+  expect(result).toBe(4).toBeA('number');
 });
+
+it('should verify that first and last names are set', () => {
+  var user = {};
+  var result = utils.setName(user, 'John Smith'); //In Javascript it is passed by reference
+  expect(result).toInclude({
+    firstName: 'John',
+    lastName: 'Smith'
+  });
+});
+
+// it('should expect some values', () => {
+//   //Uses the triple equals to check if they are the exact same object
+//   //In this case it will not work since they are two different objects
+//   expect({name: 'Kimathi'}).toBe({name: 'Kimathi'});
+//   //Uses double equals to check if they have the same properties
+//   expect({name: 'Kimathi'}).toEqual({name: 'Kimathi'});
+//   expect([2,3,4]).toInclude(2);
+// });
